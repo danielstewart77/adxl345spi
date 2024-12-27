929d8dbb63e35c31f5ce97265e5426b4f94989d1
Daniel Stewart <daniel.stewart77@gmail.com>
2024-12-27 11:22:13 -0600

    fix venv paths

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -5,7 +5,6 @@ VENV_DIR="$INSTALL_DIR/venv"
 echo "Creating a virtual environment..."
 # Create a virtual environment
 python3 -m venv "$VENV_DIR"
-source "$VENV_DIR/bin/activate"
-venv/bin/pip3 install --upgrade pip
-venv/bin/pip install -r "$INSTALL_DIR/requirements.txt"
+"$VENV_DIR/bin/pip3" install --upgrade pip
+"$VENV_DIR/bin/pip3"/bin/pip install -r "$INSTALL_DIR/requirements.txt"
 echo "Virtual environment created."
