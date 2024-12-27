c177e626bf4d2b7687285c8b2b8865200545579e
Daniel Stewart <daniel.stewart77@gmail.com>
2024-12-27 11:54:45 -0600

    ifxed branch in update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Updating RedOak..."
 cd /opt/redoak
-sudo git pull origin master
+sudo git pull origin main
 # Here you might want to include commands to recompile or reinstall dependencies if necessary
 echo "RedOak has been updated."
