a6c06be878e3fe4f2a15f740c9d0b06004c1100c
Daniel Stewart <daniel.stewart77@gmail.com>
2024-12-27 11:28:33 -0600

    updated compile

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,7 +3,7 @@ INSTALL_DIR="/opt/redoak"
 # Check if adxl345spi executable exists
 if [ ! -f "$INSTALL_DIR/adxl345spi" ]; then
     echo "adxl345spi not found, compiling..."
-    gcc "$INSTALL_DIR/adxl345spi_quad.c" -o "$INSTALL_DIR/adxl345spi" -lpigpio -pthread
+    gcc -I"$INSTALL_DIR" "$INSTALL_DIR/adxl345spi_quad.c" -o "$INSTALL_DIR/adxl345spi" -lpigpio -pthread
     if [ $? -ne 0 ]; then
         echo "Compilation failed. Exiting."
         exit 1
