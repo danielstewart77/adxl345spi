e9bc0d66f86375f53ecce57d0b099eb4cccfec5e
Daniel Stewart <daniel.stewart77@gmail.com>
2025-01-04 12:25:51 -0600

    removed the commands to remove service (no longer intstalled by default)

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "Uninstalling RedOak..."
-sudo systemctl stop redoak.service
-sudo systemctl disable redoak.service
-sudo rm -f /etc/systemd/system/redoak.service
 sudo rm -f -r /opt/redoak
 sudo rm -f /usr/local/bin/redoak-launcher
 sudo rm -f /usr/share/applications/redoak_launcher.desktop
